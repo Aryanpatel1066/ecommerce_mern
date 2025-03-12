0cce775c73d005d4a640805978b9cc09ed22bbac
aryanPatel <aryanpatel1248@gmail.com>
2025-03-12 11:15:24 +0530

    host the backend

--- frontend/src/api/apiservices.js
+++ frontend/src/api/apiservices.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:1066/ecomm/api/v1",  
+    // baseURL: "http://localhost:1066/ecomm/api/v1",  
+    baseURL:"https://ecommerce-mern-1nkl.onrender.com/ecomm/api/v1",
     withCredentials: true // Allow cookies & authentication headers
 });
 
